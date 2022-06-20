0e6a5d297561f4e68284b54053f1cb40dbca6884
AkramRickon <akramhsin29@gmail.com>
2022-06-21 02:38:19 +0600

    Html entities Problem added

---
+++ FreeCodeCamp/Intermediate Algorithm Scripting/Convert Html Entites/Convert Html entites.js
@@ -0,0 +1,13 @@
+function convertHTML(str) {
+    const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  return str.split('').map(val=> htmlEntities[val] ? htmlEntities[val]
+  : val).join('');
+}
+
+convertHTML("Dolce & Gabbana");
